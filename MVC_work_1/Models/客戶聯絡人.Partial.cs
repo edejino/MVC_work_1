08ce7372165aed65932942ffe2c7cba60b2b8c92
Jino <jino@edetw.com>
2016-03-21 23:43:32 +0800

    新增客戶資料管理、客戶聯絡人管理與客戶銀行帳戶管理頁面，並增加自訂輸入驗證屬性

---
+++ MVC_work_1/Models/客戶聯絡人.Partial.cs
@@ -0,0 +1,42 @@
+namespace MVC_work_1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(客戶聯絡人MetaData))]
+    public partial class 客戶聯絡人
+    {
+    }
+    
+    public partial class 客戶聯絡人MetaData
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int 客戶Id { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 職稱 { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        public string 姓名 { get; set; }
+        
+        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+        [Required]
+		[EmailAddress]
+        public string Email { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+		[RegularExpression(@"\d{4}-\d{6}",ErrorMessage = "手機號碼格式錯誤")]
+		//[輸入合法手機號碼(ErrorMessage = "手機號碼格式錯誤")]
+		public string 手機 { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        public string 電話 { get; set; }
+    
+        public virtual 客戶資料 客戶資料 { get; set; }
+    }
+}
