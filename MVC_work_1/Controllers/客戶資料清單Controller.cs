5d1891c2e120bbfc21bd770104d851d38d77d241
Jino <jino@edetw.com>
2016-03-22 18:17:30 +0800

    增加客戶資料清單

---
+++ MVC_work_1/Controllers/客戶資料清單Controller.cs
@@ -0,0 +1,21 @@
+﻿using MVC_work_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_work_1.Controllers
+{
+    public class 客戶資料清單Controller : Controller
+    {
+		private 客戶資料Entities db = new 客戶資料Entities();
+
+		// GET: 客戶資料清單
+		public ActionResult Index()
+        {
+			var data = db.客戶資料View;
+			return View(data);
+		}
+    }
+}
---
+++ MVC_work_1/Models/客戶資料ViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_work_1.Models
+{
+	public class 客戶資料ViewModels
+	{
+		public string 客戶名稱 { get; set; }
+	    public Nullable<int> 聯絡人數量 { get; set; }
+		public Nullable<int> 銀行帳戶數量 { get; set; }
+	}
+}
